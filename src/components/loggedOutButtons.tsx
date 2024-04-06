74e1daa2c1db45092688667b042d28cd107e4a2d
Flavio <flavionogueirabarros@gmail.com>
2024-04-06 20:30:01 -0300

    signup and login still the same temp

--- src/components/loggedOutButtons.tsx
+++ src/components/loggedOutButtons.tsx
@@ -6,7 +6,7 @@ export default function LoggedOutButton(){
              <Link href='/login' className="border rounded p-2 border-gray-200 bg-white hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50">
                 Entrar
             </Link>
-            <Link href='#' className=" border rounded p-2 bg-gray-900 text-gray-50 hover:bg-gray-900/90 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90" >
+            <Link href='/login' className=" border rounded p-2 bg-gray-900 text-gray-50 hover:bg-gray-900/90 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90" >
                 Cadastre-se
             </Link>
         </div>
