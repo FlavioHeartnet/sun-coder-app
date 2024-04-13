cc989faac9461f68409d5f7fede62b7fb751b43b
Flavio <flavionogueirabarros@gmail.com>
2024-04-13 09:08:12 -0300

    Fix checkouts when the user not logged in

--- src/app/monthlyCheckout/page.tsx
+++ src/app/monthlyCheckout/page.tsx
@@ -4,32 +4,36 @@ import { loadStripe } from "@stripe/stripe-js";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 import { getUserInfoAction } from "../actions/getUserInfoAction";
+import { useEffect } from "react";
 export default function MontlyCheckout(){
   const router = useRouter();
   const handleCheckout = async() => {
-    
     const { user_id, email, isAuth, products } = await getUserInfoAction();
     if (isAuth) {
       if(products.find((e) => e.activePriceId == process.env.NEXT_PUBLIC_MONTHLY_STRIPE_SUBSCRIPTION_PRICE_ID)){
         toast.error('Você já tem esta assinatura!');
         router.push('/');
+      }else{
+        const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+        const stripe = await stripePromise;
+        const response = await fetch(process.env.NEXT_PUBLIC_BASE_URL_API+'/api/checkout', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ priceId: process.env.NEXT_PUBLIC_MONTHLY_STRIPE_SUBSCRIPTION_PRICE_ID, userId: user_id, email: email }),
+          });
+        const session = await response.json();
+        await stripe?.redirectToCheckout({ sessionId: session.id });
       }
-      const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
-      const stripe = await stripePromise;
-      const response = await fetch(process.env.NEXT_PUBLIC_BASE_URL_API+'/api/checkout', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ priceId: process.env.NEXT_PUBLIC_MONTHLY_STRIPE_SUBSCRIPTION_PRICE_ID, userId: user_id, email: email }),
-        });
-      const session = await response.json();
-      await stripe?.redirectToCheckout({ sessionId: session.id });
+      
     }else{
       router.push('/api/auth/login?post_login_redirect_url=/monthlyCheckout');
     }
   }
-  handleCheckout();
+  useEffect(() => {
+    handleCheckout();
+  },[])
     return(
       <ValidatingOrder/>
     ) 
--- src/app/yearlyCheckout/page.tsx
+++ src/app/yearlyCheckout/page.tsx
@@ -2,7 +2,7 @@
 import ValidatingOrder from "@/components/validatingOrder";
 import { loadStripe } from "@stripe/stripe-js";
 import { useRouter } from "next/navigation";
-import { JSX, SVGProps } from "react";
+import { JSX, SVGProps, useEffect } from "react";
 import toast from "react-hot-toast";
 import { getUserInfoAction } from "../actions/getUserInfoAction";
 
@@ -15,24 +15,26 @@ export default function YearlyCheckout(){
             if(products.find((e) => e.activePriceId == process.env.NEXT_PUBLIC_YEARLY_STRIPE_SUBSCRIPTION_PRICE_ID)){
                 toast.error('Você já tem esta assinatura!');
                 router.push('/');
+            }else{
+              const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+              const stripe = await stripePromise;
+              const response = await fetch(process.env.NEXT_PUBLIC_BASE_URL_API+'/api/checkout', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({ priceId: process.env.NEXT_PUBLIC_YEARLY_STRIPE_SUBSCRIPTION_PRICE_ID, userId: user_id, email: email }),
+                });
+              const session = await response.json();
+              await stripe?.redirectToCheckout({ sessionId: session.id });
             }
-          const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
-          const stripe = await stripePromise;
-          const response = await fetch(process.env.NEXT_PUBLIC_BASE_URL_API+'/api/checkout', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({ priceId: process.env.NEXT_PUBLIC_YEARLY_STRIPE_SUBSCRIPTION_PRICE_ID, userId: user_id, email: email }),
-            });
-          const session = await response.json();
-          await stripe?.redirectToCheckout({ sessionId: session.id });
         }else{
           router.push(process.env.NEXT_PUBLIC_BASE_URL_API+'/api/auth/login?post_login_redirect_url=/yearlyCheckout');
         }
       }
-        
-    handleCheckout();
+    useEffect(() => {
+      handleCheckout();
+    },[])    
            
 
     return(
