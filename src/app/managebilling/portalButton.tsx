8fa3d80be37553e1c0358b76d94d69c93b4f3c3c
Flavio Nogueira <flavionogueirabarros@gmail.com>
2025-06-09 18:56:37 -0300

    add spinning loader

--- src/app/managebilling/portalButton.tsx
+++ src/app/managebilling/portalButton.tsx
@@ -5,34 +5,39 @@ import toast from 'react-hot-toast';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
+import { Loader2 } from 'lucide-react';
 
 export default function PortalButton({user_id}: {user_id: string}) {
   const [ pending, setPending ] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [isPlanActive, setisPlanActive] = useState(false);
 
   useEffect(()=>{
     async function getCustomerPlan(){
       setisPlanActive((await getBillingPortalData(user_id))?.plan_active);
+      setLoading(false);
     }
     getCustomerPlan();
-  }, []);
+  });
   const handleClick = async () => {
     try {
       setPending(true);
       
-      if (!user_id) {
+      if (user_id === '') {
         setPending(false);
         toast.error( 'Por favor, acesse sua conta para gerenciar sua fatura.');
       }
-      const customer = await getBillingPortalData(user_id);
-      if(customer?.plan_active){
-        const { url } = await getPortalURL(customer?.stripe_customer_id);
-        console.log("redirect:" +url)
+      const { plan_active, stripe_customer_id} = await getBillingPortalData(user_id);
+      if(plan_active){
+        const { url } = await getPortalURL(stripe_customer_id);
         setPending(false);
         window.location.href = url;
-      }else{
-        toast.error('Atualize seu plano para gerenciar sua faturamento.');
+        return
       }
+      
+      toast.error('Atualize seu plano para gerenciar sua faturamento.');
+      
       
 
     } catch (error) {
@@ -49,8 +54,15 @@ export default function PortalButton({user_id}: {user_id: string}) {
         <Image src='/gear.svg' className="dark:invert" alt={""} width={15} height={15} />
         <span>Gerenciar Assinatura</span>
       </button> 
-      : 
-        <Link className="flex items-center gap-2 text-sm font-medium [&:hover]:underline" href="#assinar">
+      : loading ?
+        <motion.div
+              animate={{ rotate: 360 }}
+              transition={{ duration: 1, repeat: Number.POSITIVE_INFINITY, ease: "linear" }}
+              className="mb-3"
+            >
+              <Loader2 className="w-8 h-8 dark" />
+        </motion.div>
+        : <Link className="flex items-center gap-2 text-sm font-medium [&:hover]:underline" href="#assinar">
               <Image src='/gear.svg' className="dark:invert" alt={""} width={15} height={15} />
               <span className="hidden md:block">Assinar Pro</span>
         </Link>
